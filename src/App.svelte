2c5c4fd8229cf991d681ba47cd26483495432203
ankitnag0 <heroankit0@gmail.com>
2023-01-04 13:35:32 +0530

    committing html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,22 @@
-<h1>Test</h1>
+<script>
+    import HowTo from "./HowTo.svelte";
+    import Timer from "./Timer.svelte";
+    
+</script>
+
+<style>
+    h1, h3{
+        text-align: center;
+    }
+</style>
+
+<h1>Handwashing App</h1>
+
+<Timer/>
+
+<HowTo/>
+
+<h3>
+    <a href="https://www.who.int/teams/integrated-health-services/infection-prevention-control">Picture Source</a>
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
+</h3>
